da9fd207d4a7d2ec265f82a4249a14525a83d7a1
Mat <matt.tan@aiqueinnovation.com>
2024-05-10 16:41:55 +0800

    Feature: HeaderBox Component
    - create reusable header component with props
    
    TotalBalanceBox Component
    
    - install and implement react-countup for animated count
    
    - install and implement chart.js and react-charths-2 for the animation charts
    
    - fixed hyrdation UI error by disabling SSR

---
+++ app/(root)/page.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import dynamic from "next/dynamic";
+
+import HeaderBox from "@/components/HeaderBox";
+const TotalBalanceBox = dynamic(() => import("@/components/TotalBalanceBox"), {
+  ssr: false,
+});
+
+const HomePage = () => {
+  const loggedIn = { firstName: "Matmercer" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
+};
+
+export default HomePage;
